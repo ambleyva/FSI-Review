5230619a4ba25133f43556587cfcdb3f21c7aaf8
ambleyva <ambleyva24@gmail.com>
2022-02-12 13:18:39 -0500

    cat button

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ let dogVoteBtn = document.createElement("button")
 dogVoteBtn.textContent = "Vote for Doggo"
 
 animals[0].append(dogVoteBtn)
+
+//create cat vote button
+let catVoteBtn = document.createElement("button")
+dogVoteBtn.textContent = "Vote for Catto"
+
+animals[0].append(catVoteBtn)

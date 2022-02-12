2e93db1a11d770b920ed7c568347d968048ab7bf
ambleyva <ambleyva24@gmail.com>
2022-02-12 13:41:53 -0500

    paste

--- script.js
+++ script.js
@@ -8,6 +8,16 @@ dogVoteBtn.textContent = "Vote for Doggo"
 
 animals[0].append(dogVoteBtn)
 
+//isolate the vote count for the dog in the voting table
+let dogVotesCell = document.querySelector("#doggo-votes");
+
+//Now, we're adding this event listener
+dogVoteBtn.addEventListener('click', function(e) {
+    let dogVotesCount = parseInt(dogVotesCell.textContent);
+    dogVotesCount = dogVotesCount + 1;
+    dogVotesCell.textContent = dogVotesCount;
+});
+
 
 //create cat vote button 
 let catVoteBtn = document.createElement('button')
